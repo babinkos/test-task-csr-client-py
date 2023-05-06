fddcfff3486013d431c509dc80f89b3def6c3362
q0s <37809416+babinkos@users.noreply.github.com>
2023-05-06 20:13:05 +0300

    k6-script.js : batch body fixed, 100% 1CPU load produced

--- k6-script.js
+++ k6-script.js
@@ -18,20 +18,20 @@ const binFiles = new SharedArray('csrs', function () {
   return arr; // f must be an array[]
 });
 
-const payloads = new SharedArray('datajsons', function () {
-  // here you can open files, and then do additional processing or generate the array with data dynamically
-  var csrStart = 1;
-  var csrEnd = 10;
-  var arr = [];
-  for (var i = csrStart; i < csrEnd + 1; i++) {
-    const f = JSON.stringify({
-      name: 'test' + i + '-' + uuidv4(),
-      csr: binFiles[i - 1],
-    });
-    arr.push(f);
-  }
-  return arr; // f must be an array[]
-});
+// const payloads = new SharedArray('datajsons', function () {
+//   // here you can open files, and then do additional processing or generate the array with data dynamically
+//   var csrStart = 1;
+//   var csrEnd = 10;
+//   var arr = [];
+//   for (var i = csrStart; i < csrEnd + 1; i++) {
+//     const f = JSON.stringify({
+//       name: 'test' + i + '-' + uuidv4(),
+//       csr: binFiles[i - 1],
+//     });
+//     arr.push(f);
+//   }
+//   return arr; // f must be an array[]
+// });
 
 const reqList = new SharedArray('requestjsons', function () {
   // here you can open files, and then do additional processing or generate the array with data dynamically
@@ -42,7 +42,10 @@ const reqList = new SharedArray('requestjsons', function () {
     const f = {
       method: 'PUT',
       url: url,
-      body: { "name": "test"+i+"-" + uuidv4(), "csr": binFiles[i - 1] },
+      body: JSON.stringify({
+        "name": "test" + i + "-" + uuidv4(),
+        "csr": binFiles[i - 1]
+      }),
       params: {
         headers: { 'Content-Type': 'application/json' },
       },
@@ -71,32 +74,53 @@ export const options = {
     my_scenario1: {
       executor: 'constant-arrival-rate',
       duration: '10s', // total duration
-      preAllocatedVUs: 210, // to allocate runtime resources     preAll
+      preAllocatedVUs: 60, // to allocate runtime resources     preAll
 
-      rate: 200, // number of constant iterations given `timeUnit`
+      rate: 50, // number of constant iterations given `timeUnit`
       timeUnit: '1s',
     },
   },
 };
 
 export default function () {
-  const req0 = {
-    method: 'GET',
-    url: url0,
-  };
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  console.log("executed :");
-  const i = Math.round(Math.random() * 9);
+  // const req0 = {
+  //   method: 'GET',
+  //   url: url0,
+  // };
+  // const params = {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // };
+  // console.log("executed :");
+  // const i = Math.round(Math.random() * 9);
 
-  console.log(JSON.parse(payloads[i]).name);
-  // http.get(url0);
-  const res = http.put(url, payloads[i], params);
-  check(res, { 'status was 200': (r) => r.status == 200 });
-  console.log("node: "+JSON.parse(res.body).node);
+  // console.log(JSON.parse(payloads[i]).name);
+  // // http.get(url0);
+  // const res = http.put(url, payloads[i], params);
+  // check(res, { 'status was 200': (r) => r.status == 200 });
+  // console.log("node: " + JSON.parse(res.body).node);
+
+  const responses = http.batch([reqList[0],reqList[1],reqList[2],
+    reqList[3],reqList[4],reqList[5],reqList[6],reqList[7],
+    reqList[8],reqList[9]]);
+  // var resStart = 0;
+  // var resEnd = 1;
+  // var arr = [];
+  // for (var i = resStart; i < resEnd + 1; i++) {
+  check(responses[0], { 'status was 200': (r) => r.status == 200 });
+  check(responses[1], { 'status was 200': (r) => r.status == 200 });
+  check(responses[2], { 'status was 200': (r) => r.status == 200 });
+  check(responses[3], { 'status was 200': (r) => r.status == 200 });
+  check(responses[4], { 'status was 200': (r) => r.status == 200 });
+  check(responses[5], { 'status was 200': (r) => r.status == 200 });
+  check(responses[6], { 'status was 200': (r) => r.status == 200 });
+  check(responses[7], { 'status was 200': (r) => r.status == 200 });
+  check(responses[8], { 'status was 200': (r) => r.status == 200 });
+  check(responses[9], { 'status was 200': (r) => r.status == 200 });
+  // };
+  // console.log("batch result: ");
+  // console.log(responses[0].body);
   // sleep(Math.random() * 2);
 }
 
